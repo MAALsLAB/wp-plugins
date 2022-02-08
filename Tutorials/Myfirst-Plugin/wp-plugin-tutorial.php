1d4a947fdae77f040bd7c4175c1717678eb184d9
MAALsLAB <maalslab@gmail.com>
2022-02-08 21:45:42 +0200

    Creating my first Plugin

---
+++ Tutorials/Myfirst-Plugin/wp-plugin-tutorial.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Plugin Name:  Plugin Tutorial
+ * Plugin URI:   https://github.com/MAALsLAB
+ * Description:  Learning how to create WordPress Plugin.
+ *               The pluin adds a message at the end of each article asking
+ *               users to fllow us in Twitter. 
+ * Version:      1.0
+ * Author:       MAAL
+ * Author URI:   https://github.com/MAALsLAB
+ * License:      CoffeeRight
+ * License URI:  https://github.com/MAALsLAB/wp-plugins/blob/main/CoffeeRight-License.txt
+ */
+
+function wp_follow_us($content)
+{
+    // Only do this when a single post is displayed
+    if (is_single()) {
+        // Message to display after the post
+        // Add URL to your Twitter
+
+        $content .= '<p class="follow-us">
+                        If you liked this article, then please follow us on 
+                        <a href="https://twitter.com/maalslab" 
+                           title="MAAL\'s LAB on Twitter" 
+                           target="_blank" 
+                           rel="nofollow">
+                            Twitter
+                        </a>.
+                    </p>';
+    }
+    // Return the content
+    return $content;
+}
+
+// Hook our function to WordPress the_content filter
+add_filter('the_content', 'wpb_follow_us');
